9211abe3fd42a674a91df4427c2e56e16eba06c2
develux <erroor234@gmail.com>
2025-01-18 18:10:59 +0500

    fog: props: Use CLO HWUI props

--- device.mk
+++ device.mk
@@ -61,6 +61,22 @@ TARGET_USE_HIDL_QTI_HEALTH := true
 # HIDL
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
+# HWUI
+PPRODUCT_SYSTEM_PROPERTIES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.vendor.perf.scroll_opt=1 \
+    ro.launcher.blur.appLaunch=0
+
 # Media
 PRODUCT_ODM_PROPERTIES += \
     media.settings.xml=/vendor/etc/media_profiles_khaje.xml
